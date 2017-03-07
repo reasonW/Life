c0a34917ab48fa361628f1e39e798569406b01b9
reasonW <charlewander@gmail.com>
2017-03-08 02:08:58 +0800

    check

--- Matlab/Random_Process/central_limit.m
+++ Matlab/Random_Process/central_limit.m
@@ -1,3 +1,6 @@
+%%验证中心极限定理，画出钟形图，并对分布进行正态分布检验
+%%
+%%
 close all
 len=10000;
 bin=50;
@@ -8,17 +11,20 @@ a3=unifrnd(0,1,1,len);
 a4=unifrnd(0,1,1,len);
 subplot(4,4,1)
 hist(a,bin)
-xlabel('无叠加')
+xlabel('one random variables')
 title('均匀分布,x~U(0,1)')
 subplot(4,4,5)
-hist(a+a1+a2,bin)
-xlabel('二次叠加')
+hist(a+a1,bin)
+xlabel('Two random variables')
 subplot(4,4,9)
 hist(a+a1+a2,bin)
-xlabel('三次叠加')
+xlabel('three random variables')
 subplot(4,4,13)
-hist(a+a1+a2+a3+a4,bin)
-xlabel('五次叠加')
+hist(a+a1+a2+a3+a4,bin)      %%钟形图
+xlabel('five random variables')
+h_a =lillietest(a+a1+a2+a3+a4) %%正态分布检验
+
+
 
 %%
 b=randn(1,len);
@@ -29,29 +35,29 @@ b4=randn(1,len);
 subplot(4,4,2)
 hist(b,bin)
 title('高斯分布,x~N(0,1),')
-xlabel('无叠加')
+xlabel('one random variables')
 h = findobj(gca,'Type','patch');
 h.FaceColor = [0 0.5 0.5];
 h.EdgeColor = 'w';
 subplot(4,4,6)
 hist(b+b1,bin)
-xlabel('二次叠加')
+xlabel('Two random variables')
 h = findobj(gca,'Type','patch');
 h.FaceColor = [0 0.5 0.5];
 h.EdgeColor = 'w';
 subplot(4,4,10)
 hist(b+b1+b2,bin)
-xlabel('三次叠加')
+xlabel('three random variables')
 h = findobj(gca,'Type','patch');
 h.FaceColor = [0 0.5 0.5];
 h.EdgeColor = 'w';
 subplot(4,4,14)
-hist(b+b1+b2+b3+b4,bin)
-xlabel('五次叠加')
+hist(b+b1+b2+b3+b4,bin)      %%钟形图
+xlabel('five random variables')
 h = findobj(gca,'Type','patch');
 h.FaceColor = [0 0.5 0.5];
 h.EdgeColor = 'w';
-
+h_b =lillietest(b+b1+b2+b3+b4) %%正态分布检验
 %%
 c=exprnd(5,1,len);
 c1=exprnd(5,1,len);
@@ -61,29 +67,29 @@ c4=exprnd(5,1,len);
 subplot(4,4,3)
 hist(c,bin)
 title('指数分布,mu=5')
-xlabel('无叠加')
+xlabel('one random variables')
 h = findobj(gca,'Type','patch');
 h.FaceColor = [0 0.5 0];
 h.EdgeColor = 'w';
 subplot(4,4,7)
 hist(c+c1,bin)
-xlabel('二次叠加')
+xlabel('Two random variables')
 h = findobj(gca,'Type','patch');
 h.FaceColor = [0 0.5 0];
 h.EdgeColor = 'w';
 subplot(4,4,11)
 hist(c+c1+c2,bin)
-xlabel('三次叠加')
+xlabel('three random variables')
 h = findobj(gca,'Type','patch');
 h.FaceColor = [0 0.5 0];
 h.EdgeColor = 'w';
 subplot(4,4,15)
-hist(c+c1+c2+c3+c4,bin)
-xlabel('五次叠加')
+hist(c+c1+c2+c3+c4,bin)      %%钟形图
+xlabel('five random variables')
 h = findobj(gca,'Type','patch');
 h.FaceColor = [0 0.5 0];
 h.EdgeColor = 'w';
-
+h_c =lillietest(c+c1+c2+c3+c4) %%正态分布检验
 
 %%
 d=chi2rnd(5,1,len);
@@ -94,25 +100,60 @@ d4=chi2rnd(5,1,len);
 subplot(4,4,4)
 hist(d,bin)
 title('卡方分布,V=5')
-xlabel('无叠加')
+xlabel('one random variables')
 h = findobj(gca,'Type','patch');
 h.FaceColor = [0 0 0.5];
 h.EdgeColor = 'w';
 subplot(4,4,8)
 hist(d+d1,bin)
-xlabel('二次叠加')
+xlabel('Two random variables')
 h = findobj(gca,'Type','patch');
 h.FaceColor = [0 0 0.5];
 h.EdgeColor = 'w';
 subplot(4,4,12)
 hist(d+d1+d2,bin)
-xlabel('三次叠加')
+xlabel('three random variables')
 h = findobj(gca,'Type','patch');
 h.FaceColor = [0 0 0.5];
 h.EdgeColor = 'w';
 subplot(4,4,16)
-hist(d+d1+d2+d3+d4,bin)
-xlabel('五次叠加')
+hist(d+d1+d2+d3+d4,bin)      %%钟形图
+xlabel('five random variables')
 h = findobj(gca,'Type','patch');
 h.FaceColor = [0 0 0.5];
 h.EdgeColor = 'w';
+h_d =lillietest(d+d1+d2+d3+d4) %%正态分布检验
+ 
+for ai=0:10000
+    a=a+unifrnd(0,1,1,len);
+    h_a_ =lillietest(a);
+    if h_a_ == 0
+        ai
+        figure
+        hist(a)
+        break;
+    end
+end
+disp('end')
+for ci=0:10000
+    c=c+exprnd(5,1,len);
+    h_c_ =lillietest(c);
+    if h_c_ == 0
+        ci
+        figure
+        hist(c)
+        break;
+    end
+end
+disp('end')
+for di=0:10000
+    d=d+chi2rnd(5,1,len);
+    h_d_ =lillietest(d);
+    if h_d_ == 0
+        di
+        figure
+        hist(d)
+        break;
+    end
+end
+disp('end')
